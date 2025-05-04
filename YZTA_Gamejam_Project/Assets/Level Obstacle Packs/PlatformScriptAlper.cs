a1acc685a8eaf1e977f58b9dbe9d8ac0f419e485
tritonicy <burakonurbey@gmail.com>
2025-05-04 15:18:14 +0300

    fix2

--- YZTA_Gamejam_Project/Assets/Level Obstacle Packs/PlatformScriptAlper.cs
+++ YZTA_Gamejam_Project/Assets/Level Obstacle Packs/PlatformScriptAlper.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 
-public class PlatformScript : MonoBehaviour
+public class PlatformScriptAlper : MonoBehaviour
 {
     [SerializeField] GameObject platform;
     [SerializeField] TextMeshProUGUI text;
---
+++ YZTA_Gamejam_Project/Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+    public Transform endPoint;
+    public Transform endPoint2;
+    public Transform endPoint3;
+    public Transform platform;
+    public Transform platform2;
+    public Transform platform3;
+
+    private Rigidbody2D buttonRb;
+
+
+    public float speed;
+    private void Start()
+    {
+        buttonRb = GetComponent<Rigidbody2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)//butona temas
+    {
+        if (collision.CompareTag("Character"))
+        {
+            if (platform != null)
+                PositionAdjuster(platform, endPoint);
+            if (platform2 != null)
+                PositionAdjuster(platform2, endPoint2);
+            if (platform3 != null)
+                PositionAdjuster(platform3, endPoint3);
+        }
+    }
+    private void Update()
+    {
+
+    }
+    private void PositionAdjuster(Transform mainPlatform, Transform endPos)
+    {
+        Vector2 targetPos = endPos.position;
+        mainPlatform.position = Vector2.Lerp(mainPlatform.position, targetPos, speed * Time.deltaTime);
+    }
+    
+}
